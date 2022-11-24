718d43cb1c4681ef6feaed66fa2be2d00b456523
PaulAdam <paul.adrian2001@yahoo.com>
2022-11-24 15:44:15 +0200

    Add extract comments

--- application/extract.py
+++ application/extract.py
@@ -12,7 +12,7 @@ def save(image, name, box, width=150, height=150):
     x, y, w, h = box
     image_crop = image[y:h, x: w]
     image_crop = cv2.resize(image_crop, (width, height))
-    cv2.imwrite(name + ".jpg", image_crop)
+    cv2.imwrite(name + ".jpg", image_crop)                          # save the new image with the face
 
 
 def face(file):
@@ -20,13 +20,13 @@ def face(file):
     faces = detector(frame)
     margin = 50
 
-    if len(faces) != 1:
+    if len(faces) != 1:                                                                             # only one face allowed
         return "Upload a valid ID Card!"
     else:
         for face in faces:
-            x1, y1 = face.left(), face.top()
+            x1, y1 = face.left(), face.top()                                                        # get face coordinates
             x2, y2 = face.right(), face.bottom()
-            save(frame, path + str(0), (x1 - margin, y1 - margin, x2 + margin, y2 + margin))
+            save(frame, path + str(0), (x1 - margin, y1 - margin, x2 + margin, y2 + margin))        # save a new picture with the face (with padding)
 
         frame = cv2.resize(frame, (800, 800))
 
